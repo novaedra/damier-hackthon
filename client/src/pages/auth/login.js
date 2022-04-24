14c6815c0aeacf415ef2d6efbb4368cb2618d9a8
BrietGame <alexis.briet2003@gmail.com>
2022-04-24 02:29:24 +0200

    Last update (really)

--- client/src/pages/auth/login.js
+++ client/src/pages/auth/login.js
@@ -18,44 +18,46 @@ const LoginPage = () => {
 
  return (
   <>
-   <div className='wrap'>
-    <div className='left'>
-     <div className='title'>
-      <h1>Se connecter</h1>
-      <div className='separator_title'></div>
-     </div>
-
-     <form>
-      <input type="text" placeholder='Identifiant(s)' />
-
-      <div className='password_section'>
-       <input id='password_input' type="password" placeholder='Mot de passe' />
-       <span id='icon_pwd' className='icon_pwd'><FontAwesomeIcon icon={faEye} className="eye_pwd" onClick={togglePwdType} /></span>
+   <div id='login'>
+    <div className='wrap'>
+     <div className='left'>
+      <div className='title'>
+       <h1>Se connecter</h1>
+       <div className='separator_title'></div>
       </div>
 
-     </form>
-     <a className='forgot_pwd' href=''>Mot de passe oublié ?</a>
+      <form>
+       <input type="text" placeholder='Identifiant(s)' />
 
-     <div className='btn_form'>
-      <div className='remember_me'>
-       <label className="container">Se souvenir de moi
-        <input type="checkbox" />
-        <span className="checkmark"></span>
-       </label>
+       <div className='password_section'>
+        <input id='password_input' type="password" placeholder='Mot de passe' />
+        <span id='icon_pwd' className='icon_pwd'><FontAwesomeIcon icon={faEye} className="eye_pwd" onClick={togglePwdType} /></span>
+       </div>
+
+      </form>
+      <a className='forgot_pwd' href=''>Mot de passe oublié ?</a>
+
+      <div className='btn_form'>
+       <div className='remember_me'>
+        <label className="container">Se souvenir de moi
+         <input type="checkbox" />
+         <span className="checkmark"></span>
+        </label>
+       </div>
+       <input type="submit" value={'Se Connecter'} onClick={() => { document.location.href = '/' }} />
       </div>
-      <input type="submit" value={'Se Connecter'} onClick={() => {document.location.href = '/'}} />
+     </div>
+     <div className='right'>
+      <div className='img'>
+       <img src='https://upload.wikimedia.org/wikipedia/commons/a/a5/Transdev_logo_2018.png' alt='Logo Transdev' />
+      </div>
      </div>
     </div>
-    <div className='right'>
-     <div className='img'>
-      <img src='https://upload.wikimedia.org/wikipedia/commons/a/a5/Transdev_logo_2018.png' alt='Logo Transdev' />
-     </div>
-    </div>
+
+    <footer>
+     Transdev &copy; 2021 - <a href='#' target='_blank'>Politique de confidentialité</a>
+    </footer>
    </div>
-
-   <footer>
-    Transdev &copy; 2021 - <a href='#' target='_blank'>Politique de confidentialité</a>
-   </footer>
   </>
  )
 }
